53a7935edfc7a6459edbc934a78be2bfbf9f4a8c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-02 11:57:38 +0000

    Deploy glikely/ebbr to github.com/glikely/ebbr.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,11 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v1.0.1-12-gb6d0ab7',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'singlehtml',
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
